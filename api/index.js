b4dc41060e984dddefc8a64097ac71f3780c679c
jhardoin <jacob_ardoin@icloud.com>
2024-06-18 17:29:15 -0500

    update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
 const Transaction = require('./models/Transaction.js');
 
